02e326445af3b257fdb9676fe4b9836d38001e19
Prerna Batra <prbatra@paypal.com>
2019-09-17 14:37:35 -0700

    formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,28 +8,49 @@ import java.util.stream.Stream;
 
 public class Main {
 
+    private static final double AGRESSIVE_RETURN = 9.4324;
+    private static final double AGRESSIVE_RISK = 15.675;
+    private static final double CONSERVATIVE_RETURN = 6.189;
+    private static final double CONSERVATIVE_RISK = 6.3438;
+    private static final int SIMULATION_COUNT = 10000;
 
     public static void main(String[] args) {
-        Random random = new Random();
-        double profit = 9.4324;
-        double risk = 15.675;
-
-//        double profit = 6.189;
-//        double risk = 6.3438;
-
-        double mean = profit / 100;
-        double std = risk / 100;
-        int simulationCount = 10000;
 
         double initialCapital = 100000.0;
 
+        //aggressive calculations
+        double[] agressiveSimulationResults = getSimulationResults(initialCapital, AGRESSIVE_RETURN / 100, AGRESSIVE_RISK/ 100, SIMULATION_COUNT);
+        double agressiveMedian = getMedian(agressiveSimulationResults, SIMULATION_COUNT);
+        double agressive90thPercentile = getPercentile(agressiveSimulationResults, 90);
+        double agressive10thPercentile = getPercentile(agressiveSimulationResults, 10);
+
+        //conservative calculations
+        double[] conservativeSimulationResults = getSimulationResults(initialCapital, CONSERVATIVE_RETURN / 100, CONSERVATIVE_RISK/ 100, SIMULATION_COUNT);
+        double conservtiveMedian = getMedian(conservativeSimulationResults, SIMULATION_COUNT);
+        double conservative90thPercentile = getPercentile(conservativeSimulationResults, 90);
+        double conservative10thPercentile = getPercentile(conservativeSimulationResults, 10);
+
+        String leftAlignFormat = "| %-20s | %-15f | %-15f | %-15f |%n";
+
+        System.out.format("+----------------------+------------------+---------------+-------------------+%n");
+        System.out.println("|   Portfolio Type     | Median 20th Year | 10% Best Case | 10% Worst Case   |");
+        System.out.format("+----------------------+------------------+---------------+-------------------+%n");
+        System.out.format(leftAlignFormat, "A-AGRESSIVE" ,  agressiveMedian, agressive90thPercentile, agressive10thPercentile);
+        System.out.format(leftAlignFormat, "I-VERY CONSERVATIVE" , conservtiveMedian, conservative90thPercentile, conservative10thPercentile);
+        System.out.format("+----------------------+------------------+---------------+-------------------+%n");
+
+    }
+
+    public static double[] getSimulationResults(double initialCapital, double mean, double standardDeviation, int simulationCount ){
+
+        Random random = new Random();
         double resultCapital = initialCapital;
 
-        NormalDistribution distribution = new NormalDistribution(mean, std);
+        NormalDistribution distribution = new NormalDistribution(mean, standardDeviation);
 
         double[] results = new double[simulationCount];
-        for (int j = 0; j < simulationCount; j ++) {
-            for(int i = 0; i < 20; i ++) {
+        for (int j = 0; j < simulationCount; j++) {
+            for (int i = 0; i < 20; i++) {
                 double probability = random.nextDouble();
                 double netProfit = distribution.inverseCumulativeProbability(probability);
                 resultCapital = resultCapital * (1 + netProfit);
@@ -39,17 +60,23 @@ public class Main {
             results[j] = resultCapital;
             resultCapital = initialCapital;
         }
+        return results;
+    }
 
-        Arrays.sort(results);
+    public static double getMedian(double[] simulationResults, int simulationCount) {
+
+        Arrays.sort(simulationResults);
         int mid = simulationCount / 2;
         int mod = (simulationCount + 1) % 2;
-        double median = (results[mid] + results[mid + mod]) / 2;
+        return (simulationResults[mid] + simulationResults[mid + mod]) / 2;
+
+    }
+
+    public static double getPercentile(double[] simulationResults, int nthPercentile) {
 
         Percentile percentile = new Percentile();
-        percentile.setData(results);
+        percentile.setData(simulationResults);
 
-        System.out.println(median);
-        System.out.println(percentile.evaluate(90));
-        System.out.println(percentile.evaluate(10));
+        return percentile.evaluate(nthPercentile);
     }
 }
